a2828ef4c6606abb8578c8d82760b7ba3817e715
Steve <stevekm@users.noreply.github.com>
2015-10-02 10:46:13 -0400

    Update and rename lftp_transfer_files.txt to lftp_transfer_files.sh

--- lftp_transfer_files.sh
+++ lftp_transfer_files.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# need to install lftp, available for Linux and OS X via homebrew
+# use it to transfer files from FTP server
+# make an FTP server on your Android phone with ES File Manager
 #### make sure the ftp server is set to start at phone's system root folder
 
 lftp -e "mirror --continue --only-newer --parallel=5 --no-empty-dirs -v /storage/emulated/0/ /home/steve/Samsung_Galaxy_S4_backup/storage/emulated/0/ &
@@ -10,9 +14,9 @@ lftp -e "mirror --continue --only-newer --parallel=5 --no-empty-dirs -v /storage
 
 ####
 # send files, to Nexus 6
-lftp -e "mirror --continue --only-newer --parallel=5 --no-empty-dirs -R -v /Users/kellys04/send_to_phone/ Music" ftp://10.184.13.102:3721/
+lftp -e "mirror --continue --only-newer --parallel=5 --no-empty-dirs -R -v /Users/kellys04/send_to_phone/ Music" ftp://xx.xx.xx.xx:xx/
 
 ###
 
 lftp -e "mirror -R {local dir} {remote dir}" -u {username},{password} {host}
-lftp -e "mirror --dry-run --continue --only-newer --parallel=5 --no-empty-dirs -R -v /Users/USERNAME/send_to_phone/ Music" ftp://10.184.13.102:3721/
+lftp -e "mirror --dry-run --continue --only-newer --parallel=5 --no-empty-dirs -R -v /Users/USERNAME/send_to_phone/ Music" ftp://xx.xx.xx.xx:xx/
