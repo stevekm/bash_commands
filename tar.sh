eec1014b14985efdeae8a9479154eb6faedfcbf0
Steve <stevekm@users.noreply.github.com>
2015-10-02 10:32:52 -0400

    Update and rename tar.txt to tar.sh

---
+++ tar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+tar -cvzf /path/to/output.tar.gz /path/to/input/directory_or_files
+# c = create
+# v = verbose
+# z = gzip'd
+# f = ...output file?
+
+(trusty)steve@localhost:~$ tar -cvzf /media/removable/USB\ Drive/my_stuff.tar.gz /media/removable/USB\ Drive\ 1/my_stuff/
+
