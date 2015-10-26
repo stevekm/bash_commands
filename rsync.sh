8d8f73091e975fc32860d02abfbc03159ba4b91b
Steve <stevekm@users.noreply.github.com>
2015-10-26 13:26:15 -0400

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-rsync -avzhR --progress /media/steve/disk/Music/ /media/steve/31F0895514032E83/Music/
+
+# rsync {source} {target} 
+
+rsync -avhR --progress /media/steve/disk/Music/ /media/steve/31F0895514032E83/Music/
 # a =  archive mode, preserve metadata
 # v =  verbose
-# z = compression (good for network transfer)
-# h = human readable ?
-# R = recursive? 
+# z = compression (don't use for local transfers)
+# h = human readable 
+# R = relative path names (why??)
+
+# to/from a remote server..
+
+rsync -avzheR --progress -e "ssh" /Users/steve/local/dir steve@server.com:/path/to/remote/dir
